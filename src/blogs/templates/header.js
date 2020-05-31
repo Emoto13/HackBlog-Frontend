d54fd851157a558ebac23bd5a92c68b79c3d9928
Emoto13 <emcho_spassov@abv.bg>
2020-05-31 18:13:43 +0300

    Add

---
+++ src/blogs/templates/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from './logo.js'
+import NavigationBar from './navigationBar.js'
+
+function Header(){
+    return (
+    <div>
+        <Logo />
+        <NavigationBar />
+    </div>
+    )
+}
+
+export default Header
