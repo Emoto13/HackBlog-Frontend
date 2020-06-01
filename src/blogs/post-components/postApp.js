13b64f9726183fa02c009636af00bc3a3571bcd8
Emoto13 <emcho_spassov@abv.bg>
2020-06-01 22:31:22 +0300

    Add  component to

--- src/blogs/post-components/postApp.js
+++ src/blogs/post-components/postApp.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PostRenderer from './postRenderer'
+import Header from '../templates/header.js'
 
 function PostApp(props){
     return (
     <div>
+        <Header />
         <PostRenderer url_id={props.match.params.id} type={props.match.params.typePost}/>
     </div>
     )
