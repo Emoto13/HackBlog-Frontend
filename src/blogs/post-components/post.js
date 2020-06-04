a78dc3f3734185e480a87769814191b66299ecc9
Nikolai Kasev <n.kasev98@gmail.com>
2020-06-04 15:57:52 +0300

    Refactored .js files

--- src/blogs/post-components/post.js
+++ src/blogs/post-components/post.js
@@ -1,14 +1,15 @@
 import React from "react";
+import "./styles.css"
 
 
 function Post(props){
     return (
-    <div>
-        <h1>{props.title}</h1>
-        <img src={props.image} alt={props.title}/>
-        <p>{props.content}</p>
-        <div>{props.date}</div>
-        <div>{props.author_name}</div>
+    <div className="div_post">
+        <h1 className='title_post'>{props.title}</h1>
+        <div className='date_post'>Published on: {props.date}</div>
+        <div className='author_name_post'>Author: {props.author_name}</div>
+        <img src={props.image} alt={props.title} className='image_post'/>
+        <p className='p_post'>{props.content}</p>
     </div>
     )
 }
