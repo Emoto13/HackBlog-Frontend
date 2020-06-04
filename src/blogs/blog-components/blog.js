b0fe4a13f94e6cac5b25cbbb0babc25572b4c451
Emoto13 <emcho_spassov@abv.bg>
2020-06-04 16:43:38 +0300

    Change styles

--- src/blogs/blog-components/blog.js
+++ src/blogs/blog-components/blog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles.css'
+import './styles-blog.css'
 
 function Blog(props){
     const content = props.content.slice(0, 350)
--- src/blogs/post-components/post.js
+++ src/blogs/post-components/post.js
@@ -1,15 +1,15 @@
 import React from "react";
-import "./styles.css"
+import "./styles-post.css"
 
 
 function Post(props){
     return (
-    <div className="div_post">
-        <h1 className='title_post'>{props.title}</h1>
-        <div className='date_post'>Published on: {props.date}</div>
-        <div className='author_name_post'>Author: {props.author_name}</div>
-        <img src={props.image} alt={props.title} className='image_post'/>
-        <p className='p_post'>{props.content}</p>
+    <div className="divPost">
+        <h1 className='titlePost'>{props.title}</h1>
+        <div className='datePost'>Published on: {props.date}</div>
+        <div className='authorNamePost'>Author: {props.author_name}</div>
+        <img src={props.image} alt={props.title} className='imagePost'/>
+        <div className='contentPost'>{props.content}</div>
     </div>
     )
 }
