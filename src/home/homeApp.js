675cc51fda3eeb6ba10ff540b6a9b125618914e2
Emoto13 <emcho_spassov@abv.bg>
2020-06-05 10:09:54 +0300

    Add

--- src/home/homeApp.js
+++ src/home/homeApp.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import Gallery from './gallery.js'
-import MiniBlogList from './miniBlogList.js'
 import Header from '../blogs/templates/header.js'
+import Slideshow from './slideShow.js'
+
 
 function HomeApp(props){
     return (
         <div>
             <Header />
-            <MiniBlogList type="sport" />
-            <MiniBlogList type="politics" />
-            <MiniBlogList type="tech" />
-            <MiniBlogList type="opinion" />
-            <MiniBlogList type="other" />
+            <Slideshow />
         </div>
     )
 }
