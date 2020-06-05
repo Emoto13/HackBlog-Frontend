578a5053868b708600c3700eb1e85d34578deca5
Nikolai Kasev <n.kasev98@gmail.com>
2020-06-05 11:58:36 +0300

    Refactored styles and added to title

--- src/home/blogSlide.js
+++ src/home/blogSlide.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function BlogSlide(props){
     const linkTo = `/sport-post/${props.id}`
     return (
-        <Link to={linkTo} >
+        <Link className='slidelink' to={linkTo} >
             <div className="each-slide">
                 <div className="miniBlogImg" style={{'backgroundImage': `url(${props.image})`}}>
                 <span>{props.title}</span>
